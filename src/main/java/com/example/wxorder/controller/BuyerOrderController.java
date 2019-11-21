ce1adf49c3826357c09af472ecb5dce01e51e42a
wangxxxlll <1941779441@qq.com>
2019-11-21 08:03:00 +0800

    a

--- src/main/java/com/example/wxorder/controller/BuyerOrderController.java
+++ src/main/java/com/example/wxorder/controller/BuyerOrderController.java
@@ -1,16 +1,26 @@
 package com.example.wxorder.controller;
 
+import com.example.wxorder.converter.OrderForm2OrderDTOConverter;
 import com.example.wxorder.dto.OrderDTO;
+import com.example.wxorder.entity.OrderDetail;
 import com.example.wxorder.enums.ResultEnum;
 import com.example.wxorder.exception.SellException;
 import com.example.wxorder.form.OrderForm;
+import com.example.wxorder.service.OrderService;
+import com.example.wxorder.utils.ResultVOUtil;
 import com.example.wxorder.vo.ResultVo;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,6 +32,8 @@ import java.util.Map;
 @Slf4j
 @RequestMapping("/buyer/order")
 public class BuyerOrderController {
+    @Autowired
+    private OrderService orderService;
     //创建订单
     @RequestMapping("/create")
     public ResultVo<Map<String,String>> create(@Valid OrderForm orderForm,
@@ -31,12 +43,54 @@ public class BuyerOrderController {
             throw new SellException(ResultEnum.PARAM_ERROR.getCode()
                     ,bindingResult.getFieldError().getDefaultMessage());
         }
-        OrderDTO orderDTO=OrderDTO;
+        OrderDTO orderDTO= OrderForm2OrderDTOConverter.convert(orderForm);
 
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+            log.error("[创建订单] 购物车不能为空");
+            throw new SellException(ResultEnum.CART_EMPTY);
+        }
+        OrderDTO createResult=orderService.create(orderDTO);
+        Map<String,String> map=new HashMap<>();
+        map.put("orderId",createResult.getOrderId());
+        return ResultVOUtil.success(map);
 
     }
     //订单列表
+    @GetMapping("/list")
+    public ResultVo<List<OrderDetail>> list(@RequestParam("openid") String openid,
+                                            @RequestParam(value = "page",defaultValue = "0")Integer page,
+                                            @RequestParam(value = "size",defaultValue = "10")Integer size){
+        if(StringUtils.isEmpty(openid)){
+            log.error("[查询订单列表] openid为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        PageRequest pageRequest=PageRequest.of(page,size);
+        Page<OrderDTO> orderDTOPage=orderService.findList(openid,pageRequest);
+        return ResultVOUtil.success(orderDTOPage.getContent());
+    }
     //订单详情
+    @GetMapping("/detail")
+    public ResultVo<OrderDTO> detail(
+            @RequestParam("openid") String openid,
+            @RequestParam("orderId") String orderId){
+        if(StringUtils.isEmpty(openid)||StringUtils.isEmpty(orderId)){
+            log.error("[查询订单详情] openid为空或orderId为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        OrderDTO orderDTO =orderService.findOne(orderId);
+        return ResultVOUtil.success(orderDTO);
+    }
     //取消订单
+    @PostMapping("/cancel")
+    public ResultVo cancel(@RequestParam("openid") String openid,
+                           @RequestParam("orderId") String orderId){
+        if(StringUtils.isEmpty(openid)||StringUtils.isEmpty(orderId)){
+            log.error("[查询订单详情] openid为空或orderId为空");
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        OrderDTO orderDTO=orderService.findOne(orderId);
+        orderService.cancel(orderDTO);
+        return ResultVOUtil.success();
+    }
 
 }
--- src/main/java/com/example/wxorder/converter/OrderForm2OrderDTOConverter.java
+++ src/main/java/com/example/wxorder/converter/OrderForm2OrderDTOConverter.java
@@ -1,13 +1,23 @@
 package com.example.wxorder.converter;
 
 import com.example.wxorder.dto.OrderDTO;
+import com.example.wxorder.entity.OrderDetail;
+import com.example.wxorder.enums.ResultEnum;
+import com.example.wxorder.exception.SellException;
 import com.example.wxorder.form.OrderForm;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Auther: 李清依
  * @Date: 2019/11/17 15:08
  * @Description:
  */
+@Slf4j
 public class OrderForm2OrderDTOConverter {
     public static OrderDTO convert(OrderForm orderForm){
         OrderDTO orderDTO=new OrderDTO();
@@ -16,6 +26,17 @@ public class OrderForm2OrderDTOConverter {
         orderDTO.setBuyerAddress(orderForm.getAddress());
         orderDTO.setBuyerOpenid(orderForm.getOpenid());
         //购物车
+        Gson gson=new Gson();
+        List<OrderDetail> orderDetailList=new ArrayList<>();
+        try {
+            orderDetailList=gson.fromJson(orderForm.getItems(),
+                    new TypeToken<List<OrderDetail>>(){}.getType());
 
+        }catch (Exception e){
+            log.error("[对象转换] 错误，json={}",orderForm.getItems());
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+        return orderDTO;
     }
 }
